a8fb66bcc468b9ea1e3b29cf28ad4936885c7a34
CooperHub <phupha.p5@gmail.com>
2025-10-04 00:49:06 +0700

    Add files via upload

---
+++ config map.lua
@@ -0,0 +1,10 @@
+--map plant vs brainrot
+
+_G.Key = "ve3pw5hyT3jaHZDNSSJCGPVlRwUjVhjn"
+
+_G.config = {
+    ["BuySeed"] = true,
+    ["BuyGear"] = true,
+    ["AutoHitAllEnemies"] = false,
+    ["CollectMoney"] = true
+}
